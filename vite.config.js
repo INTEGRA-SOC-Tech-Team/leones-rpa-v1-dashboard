b7297d4b693fdf4b40a839b1e8c31836594ff514
axelsocdev <agutierrez@socasesores.com.mx>
2025-10-17 12:44:39 -0600

    [CONFIG] Vite cnfg

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/leones-dashboard/',  // 👈 importante: todas las rutas serán relativas a /frontend
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.integraso.com',   // tu backend real
+        target: 'https://api.integraso.com',  // sigue siendo útil para desarrollo local
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
